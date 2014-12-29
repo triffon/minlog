06fab7241bcb73f2a2f17e2b1689dd10e77f5fd0
Kenji Miyamoto <miyamoto@mathematik.uni-muenchen.de>
2014-12-29 23:10:26 +0100

    Startup script changed

--- src/minlog.el
+++ src/minlog.el
@@ -1,74 +1,73 @@
-;; Enable utf-8
-(setq default-enable-multibyte-characters t)
-(condition-case nil
-    (set-language-environment "Greek")
-  (error nil))
-(condition-case nil
-    (set-language-environment "utf-8")
-  (error nil))
-(set-input-method "TeX")
+(defun run-minlog (&optional filename)
+  ;; Enable utf-8
+  (setq default-enable-multibyte-characters t)
+  (condition-case nil
+      (set-language-environment "Greek")
+    (error nil))
+  (condition-case nil
+      (set-language-environment "utf-8")
+    (error nil))
+  (set-input-method "TeX")
 
-;; Start MINLOG
-(interactive)
+  ;; Start MINLOG
+  (interactive)
 
-; MINLOG directory
-(if (getenv "MINLOGPATH")
-    (setq minlogpath (getenv "MINLOGPATH"))
-  (setq minlogpath "---MINLOGPATH---"))
+  ;; MINLOG directory
+  (if (getenv "MINLOGPATH")
+      (setq minlogpath (getenv "MINLOGPATH"))
+    (setq minlogpath "---MINLOGPATH---"))
 
-; which scheme to use ?
-(cond ((getenv "SCHEME") (setq scheme (getenv "SCHEME")))
-      ((and (file-readable-p "/usr/bin/petite")
-            (string= system-type "gnu/linux"))
-       (setq scheme "/usr/bin/petite"))
-      ((eq 0 (shell-command "which petite"))
-       (setq scheme "petite"))
-      ((eq 0 (shell-command "which mzscheme"))
-       (if (string-match "v4." (shell-command-to-string "mzscheme --version"))
-	   (setq scheme "mzscheme -l mzscheme -l r5rs -i --load")
-	 (setq scheme "mzscheme --load")))
-      ((eq 0 (shell-command "which guile"))
-       (setq scheme "guile -l"))
-      (t (error "Neither petite nor mzscheme nor guile installed ")))
+  ;; which scheme to use ?
+  (cond ((getenv "SCHEME") (setq scheme (getenv "SCHEME")))
+	((and (file-readable-p "/usr/bin/petite")
+	      (string= system-type "gnu/linux"))
+	 (setq scheme "/usr/bin/petite"))
+	((eq 0 (shell-command "which petite"))
+	 (setq scheme "petite"))
+	((eq 0 (shell-command "which mzscheme"))
+	 (if (string-match "v4." (shell-command-to-string "mzscheme --version"))
+	     (setq scheme "mzscheme -l mzscheme -l r5rs -i --load")
+	   (setq scheme "mzscheme --load")))
+	((eq 0 (shell-command "which guile"))
+	 (setq scheme "guile -l"))
+	(t (error "Neither petite nor mzscheme nor guile installed ")))
 
+  ;; setup the frame
+  (setq inhibit-startup-message t)
+  (delete-other-frames)
+  (delete-other-windows)
+  (let* ((orig-frame (selected-frame))
+	 (left-frame (make-frame))
+	 (right-frame (make-frame))
+	 (wh (frame-pixel-width left-frame))
+	 (left-frame-lt (frame-parameter left-frame 'left))
+	 (lt (if (< (+ left-frame-lt (* wh 2)) (x-display-pixel-width))
+		 left-frame-lt 0))
+	 (ht (/ (x-display-pixel-height) (frame-char-height)))
+	 (border (frame-parameter left-frame 'border-width))
+	 (left-params (list (cons 'left lt)
+			    (cons 'top 0)
+			    (cons 'height ht)))
+	 (right-params (list (cons 'left (+ lt wh (* 2 border)))
+			     (cons 'top 0)
+			     (cons 'height ht))))
 
-; is there a heap file ?
-(if (and (file-readable-p (concat minlogpath "/minlog.heap"))
-         (or (string= scheme "petite")
-             (string= scheme "/usr/bin/petite")))
-    (setq heapload
-          (concat "-h " minlogpath "/minlog.heap " minlogpath "/welcome.scm"))
-  (setq heapload (concat minlogpath "/init.scm")))
+    ;; is there a heap file ?
+    (if (and (file-readable-p (concat minlogpath "/minlog.heap"))
+	   (or (string= scheme "petite")
+	       (string= scheme "/usr/bin/petite")))
+	(setq heapload
+	      (concat "-h " minlogpath "/minlog.heap "
+		      minlogpath "/welcome.scm"))
+      (setq heapload (concat minlogpath "/init.scm")))
 
+    (modify-frame-parameters left-frame left-params)
+    (modify-frame-parameters right-frame right-params)
+    (delete-frame orig-frame)
 
-(run-scheme(concat scheme " " heapload))
+    ;; start scheme
+    (run-scheme (concat scheme " " heapload))
 
-;; set-up windows
-(delete-other-windows)
-(split-window-vertically)
-(other-window 1)
-(set-buffer "*scheme*")
-(rename-buffer "*minlog*" t)
-(setq scheme-buffer "*minlog*")
-(other-window 1)
-(let ((text (concat
-             "\t\t Minlog loading...\n\n\t\t To "
-             (propertize "load" 'face '(foreground-color . "green"))
-             " a file use "
-             (propertize "Control-x Control-f" 'face '(foreground-color . "red"))
-             ", \n\t\t to "
-             (propertize "save" 'face '(foreground-color . "green"))
-             " your minlog script in a new file \n\t\t use "
-             (propertize "Control-x Control-w" 'face '(foreground-color . "red"))
-             ", \n\t\t to "
-             (propertize "begin" 'face '(foreground-color . "green") )
-             " a new script just type in."))
-      (splash-buffer (generate-new-buffer "Minlog loading...")))
-  (switch-to-buffer splash-buffer)
-  (insert "\n\n\n\n\n\n\n\n\n" text)
-  (set-buffer-modified-p nil)
-  (sit-for 100000) ; wait until eternity or a keypress
-  (kill-buffer splash-buffer)
-  (switch-to-buffer (get-buffer "*scratch*"))
-  (scheme-mode)
-  )
+    ;; open file
+    (select-frame-set-input-focus right-frame)
+    (if filename (find-file filename))))
