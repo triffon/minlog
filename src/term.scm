6a2d48773e522215b7cf0be06d2449299201e92e
Helmut Schwichtenberg <schwicht@math.lmu.de>
2015-06-11 21:19:44 +0200

    nbe-term-to-object corrected in case term-in-if-form.  There is a special treatment for an if term with the same alternatives, so that the term reduces to an alternative without checking the condition.  In order to check the equality of the alternatives, they are normalized and referred to by norm-alts in the source code.  The old implementation unexpectedly used this norm-alts to compute the final result.  It repeated substitution once more using the same variable bindings, and caused a wrong result (problem observed by Masahiko Sato, correction done to Kenji Miyamoto).  (Correction was already done 2014-12-19, but somehow got lost)

--- src/term.scm
+++ src/term.scm
@@ -3915,7 +3915,8 @@ intDestr n | n > 0  = Left n
 		(nbe-term-to-object test bindings))
 	       ((constant-alts? norm-alts typed-constr-names)
 		(nbe-term-to-object
-		 (constants-alts-to-const-term norm-alts typed-constr-names)
+		 (constants-alts-to-const-term alts typed-constr-names)
+		 ;; (constants-alts-to-const-term norm-alts typed-constr-names)
 		 bindings))
 	       (else
 		(nbe-reflect
