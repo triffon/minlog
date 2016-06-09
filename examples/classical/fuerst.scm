00b008811196d82d422152efebbb4189d3f5d626
Helmut Schwichtenberg <schwicht@math.lmu.de>
2016-06-09 17:23:32 +0200

    Incorporated some simplifications from Dominique Larchey-Wendling in his Coq-proof in archive/larchey/fuerst.v

--- examples/classical/fuerst.scm
+++ examples/classical/fuerst.scm
@@ -1,4 +1,6 @@
-;; 2016-04-09.  fuerstenberg.scm.  Based on temp/fuerstenberg.scm.
+;; 2016-06-09.  fuerst.scm.  Based on temp/fuerstenberg.scm.
+;; Incorporatimg some simplifications from Dominique Larchey-Wendling
+;; in his Coq-proof in archive/larchey/fuerst.v
 
 ;; Extraction of computational content from Fuerstenberg's topological
 ;; proof of the existence of infinitely many primes.  This is the
@@ -30,7 +32,7 @@
 
 ;; The extracted term will be [p,n]Succ(Prod n p)
 
-(add-var-name "l" "b" "x" (py "nat"))
+(add-var-name "b" "x" (py "nat"))
 (add-var-name "p" (py "nat=>nat")) ;primes
 
 (add-program-constant "Prod" (py "nat=>(nat=>nat)=> nat"))
@@ -57,7 +59,7 @@
 (save-totality)
 
 ;; ProdPerm
-(set-goal "all p,m,l(l<m -> exl k p l*k=Prod m p)")
+(set-goal "all p,m,l(l<m -> exl n p l*n=Prod m p)")
 (assume "p")
 (ind)
 ;; Base
@@ -73,10 +75,10 @@
 (assume "l<m")
 (inst-with-to "IH" (pt "l") "l<m" "IHInst")
 (drop "IH")
-(by-assume "IHInst" "k1" "k1Prop")
-(intro 0 (pt "k1*p m"))
+(by-assume "IHInst" "n1" "n1Prop")
+(intro 0 (pt "n1*p m"))
 (ng)
-(simp "k1Prop")
+(simp "n1Prop")
 (use "Truth")
 ;; Case l=m
 (assume "l=m")
@@ -112,61 +114,37 @@
 (save "NatLeOneProd")
  
 ;; PrimeProdNotOne
-(set-goal "all p,m(all l(l<m -> 1<p l) -> all l,k(l<m -> 1=p l*k -> F))")
-(assume "p" "m" "1<ps" "l")
+(set-goal "all x,n(1<x -> 1=x*n -> F)")
+(assume "x")
 (cases)
-;; k=0
-(assume "l<m" "Absurd")
+(assume "1<x" "Absurd")
 (use "Absurd")
-;; Successor
+(assume "n" "1<x")
 (ng)
-(assert "all k(l<m -> 1+1<=p l*k+p l)")
- (cases)
- ;; k=0
- (assume "l<m")
- (ng)
- (use "NatLtToSuccLe")
- (use "1<ps")
- (use "l<m")
- ;; Successor
- (assume "k" "l<m")
+(assume "1=x*n+x")
+(simphyp-with-to "1<x" "1=x*n+x" "HypSimp")
+(assert "0+x<x")
+ (use "NatLeLtTrans" (pt "x*n+x"))
  (use "NatLeMonPlus")
- (ng)
- (use "NatLeTrans" (pt "p l"))
- (use "NatLtToLe")
- (use "1<ps")
- (use "l<m")
  (use "Truth")
- (use "NatLtToLe")
- (use "1<ps")
- (use "l<m")
-(assume "Assertion" "k" "l<m" "EqHyp")
-(assert "1+1<=p l*k+p l")
- (use "Assertion")
- (use "l<m")
- (simp "<-" "EqHyp")
+ (use "Truth")
+ (use "HypSimp")
 (assume "Absurd")
 (use "Absurd")
 ;; Proof finished.
 (save "PrimeProdNotOne")
 
 ;; UnionNpsUnifClosed
-;; all m exl b(0<b andu all x(x in ~U_{l<m}Np_l -> all n x+b*n in ~U_{l<m}Np_l))
-;; where x in ~U_{l<m}Np_l means all_{l<m} all k(x=p l*k -> F)
-
-(set-goal "all p,m(all l(l<m -> 1<=p l) -> exl b(1<=b andu all x(
- all l,k(l<m -> x=p l*k -> F) ->
- all n,l,k(l<m -> x+b*n=p l*k -> F))))")
+(set-goal "all p,m(all l(l<m -> 1<=p l) -> exl b(all x(
+ all l,n1(l<m -> x=p l*n1 -> F) ->
+ all n,l,n1(l<m -> x+b*n=p l*n1 -> F))))")
 (assume "p" "m" "1<=ps")
 (intro 0 (pt "Prod m p"))
-(split)
-(use "NatLeOneProd")
-(use "1<=ps")
-(assume "x" "xnotinU" "n" "l" "k" "l<m")
+(assume "x" "xnotinU" "n" "l" "n1" "l<m")
 (inst-with-to "ProdPerm" (pt "p") (pt "m") (pt "l") "l<m" "ProPermInst")
-(by-assume "ProPermInst" "k1" "k1Prop")
-(simp "<-" "k1Prop")
-(inst-with-to "xnotinU" (pt "l") (pt "k--k1*n") "l<m" "xnotinUInst")
+(by-assume "ProPermInst" "n2" "n2Prop")
+(simp "<-" "n2Prop")
+(inst-with-to "xnotinU" (pt "l") (pt "n1--n2*n") "l<m" "xnotinUInst")
 (drop "xnotinU")
 (assume "EqHyp")
 (use "xnotinUInst")
@@ -178,12 +156,14 @@
 
 ;; mPrimesDontSuffice
 ;; Assuming 1<p l (l<m) there is an x with x in ~U_{l<m}Np_l
-(set-goal "all p,m(all l(l<m -> 1<p l) -> exl x all l,k(l<m -> x=p l*k -> F))")
+(set-goal "all p,m(all l(l<m -> 1<p l) -> exl x all l,n(l<m -> x=p l*n -> F))")
 (assume "p" "m" "1<ps")
 ;; First show 1notinU
-(assert "all l,k(l<m -> 1=p l*k -> F)")
+(assert "all l,n(l<m -> 1=p l*n -> F)")
+ (assume "l" "n" "l<m")
  (use "PrimeProdNotOne")
  (use "1<ps")
+ (use "l<m")
 (assume "1notinU")
 (assert "all l(l<m -> 1<=p l)")
  (assume "l" "l<m")
@@ -194,11 +174,8 @@
 (inst-with-to "UnionNpsUnifClosed" (pt "p") (pt "m") "1<=ps" "bEx")
 (by-assume "bEx" "b" "bProp")
 (intro 0 (pt "1+b"))
-(assume "l" "k" "l<m" "EqHyp")
-(elim "bProp")
-(assume "Useless" "bPropRight")
-(drop "bProp" "Useless")
-(use "bPropRight" (pt "1") (pt "1") (pt "l") (pt "k"))
+(assume "l" "n" "l<m" "EqHyp")
+(use "bProp" (pt "1") (pt "1") (pt "l") (pt "n"))
 (use "1notinU")
 (use "l<m")
 (use "EqHyp")
@@ -215,3 +192,4 @@
 (define neterm (rename-variables (nt eterm)))
 (pp neterm)
 ;; [p,n]Succ(Prod n p)
+
